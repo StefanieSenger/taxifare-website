f13548f81ab4471e70cb31e121ea6df23927b96d
Stefanie Senger <stefanie.senger@posteo.de>
2022-08-26 15:23:32 +0200

    code ready to be deployed to production

--- app.py
+++ app.py
@@ -1,46 +1,30 @@
+from operator import index
 import streamlit as st
 import requests
 from datetime import datetime
+import folium
+import pandas as pd
 
 '''
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
 '''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
+## Input form to get your predicted fare
 '''
-# title = st.text_input('Movie title', 'Life of Brian')
-pickup_date_input = st.text_input('date and time')
+st.markdown('Please insert date and time in the format %Y-%m-%d %H:%M:%S')
+pickup_date_input = st.text_input('date and time', '2022-08-26 00:00:00')
 pickup_date_input = datetime.strptime(str(pickup_date_input), '%Y-%m-%d %H:%M:%S')
-pickup_lon_input = st.text_input('pickup longitude')
-pickup_lat_input = st.text_input('pickup latitude')
-dropoff_lon_input = st.text_input('dropoff longitude')
-dropoff_lat_input = st.text_input('dropoff latitude')
-passenger_input = st.text_input('passenger count')
-
+pickup_lon_input = st.text_input('pickup longitude', '-73.5758')
+pickup_lat_input = st.text_input('pickup latitude', '40.4657')
+dropoff_lon_input = st.text_input('dropoff longitude', '-74.0243')
+dropoff_lat_input = st.text_input('dropoff latitude', '40.4124')
+passenger_input = st.text_input('passenger count', '1')
 
 '''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
+## API to retrieve a prediction
 '''
-
+st.markdown('This is the pure requests.get(url).json() response:')
 url = 'https://taxifare.lewagon.ai/predict'
 url = f'https://taxifare.lewagon.ai/predict?pickup_datetime={pickup_date_input}&pickup_longitude={pickup_lon_input}\
     &pickup_latitude={pickup_lat_input}&dropoff_longitude={dropoff_lon_input}&dropoff_latitude={dropoff_lat_input}&passenger_count={passenger_input}'
@@ -48,19 +32,21 @@ url = f'https://taxifare.lewagon.ai/predict?pickup_datetime={pickup_date_input}&
 response = requests.get(url).json()
 st.write('resonse:',response)
 
-
-
-st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
 '''
-
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
 
 ## Finally, we can display the prediction to the user
 '''
-st.write('Dear user, you prediced fare is:',response['fare'])
+st.write('Dear user, your predicted fare is:',response['fare'])
+
+# displaying folium maps doesn't work on all browser types
+# m = folium.Map(location=[45.5236, -122.6750], tiles='OpenStreetMap', zoom_start=11)
+# m
+
+@st.cache
+def get_map_data():
+    return pd.DataFrame.from_dict({'lon': [float(pickup_lon_input), float(dropoff_lon_input)], 'lat': [float(pickup_lat_input), float(dropoff_lat_input)]})
+
+df = get_map_data()
+
+st.map(df)
+st.write('You will land right in the atlantic ocean, if you should be naive enough to follow this app s directive ...')
